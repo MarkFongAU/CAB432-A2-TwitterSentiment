6d187f997c007f5e1326598edebcde5a3b4f3e34
Yee Chen Fong <yeechen.fong@connect.qut.edu.au>
2017-10-05 22:04:00 +1000

    Index page layout

---
+++ app/twitter/config.js
@@ -0,0 +1,17 @@
+(function () {
+    "use strict";
+
+    /*exports.consumer_key = process.env.TWITTER_CONSUMER_KEY || 'UNMIxvXT39BBfOYSwLnhdtrZ3';
+    exports.consumer_secret = process.env.TWITTER_CONSUMER_SECRET || 'Jt0qEJoau7tZZ2W0EKdQ8w7qjF9i5mvx20Rpk1YKB0kkjQjT4o';
+    exports.access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY || '1492209756-y6m1pOIH2ZlAQoW21nmgLqPQINW5fvAgd0NhueP';
+    exports.access_token_secret =  process.env.TWITTER_ACCESS_TOKEN_SECRET || 'PvVAiEChADds66K6bC25VOh804VPAXZ549cuxQRcuiVi1';*/
+
+    exports.consumer_key = process.env.TWITTER_CONSUMER_KEY || 'Ooxs6fEprKBiK1wIf8CNLrqOR';
+    exports.consumer_secret = process.env.TWITTER_CONSUMER_SECRET || 'dkkTbEk0BwSp7h4hlt8DPJIUJ9i6x3R5L11bddNMJkIQxiepRK';
+    exports.access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY || '788633322025025536-p68oSmrGFhnepJtssMqgDsC1558KaCl';
+    exports.access_token_secret =  process.env.TWITTER_ACCESS_TOKEN_SECRET || 'XOkDtCrJPuvRFHH5Rp2kCHu2ATc9DNopAFmlbHQF4y3Tt';
+
+
+
+
+})();
