4874a6f930573066bb04c5e3918909c4f69536e2
Yee Chen Fong <yeechen.fong@connect.qut.edu.au>
2017-10-08 12:57:25 +1000

    Twitter API

---
+++ app/twitter/twitterapiconfig.js
@@ -0,0 +1,7 @@
+(function () {
+    "use strict";
+    exports.consumer_key = process.env.TWITTER_CONSUMER_KEY || 'NlOMNwv4bL201ZwN7yJShtHOa';
+    exports.consumer_secret = process.env.TWITTER_CONSUMER_SECRET || 'rcAAXY6LbOruQlBTWskxhZ8N5SNrII1Tm6MVsEp1vas63t23Xb';
+    exports.access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY || '2315083052-zojLYhSLN4DE3Z87np9sp8eyk7X8ZoDGTmEqkPB';
+    exports.access_token_secret =  process.env.TWITTER_ACCESS_TOKEN_SECRET || 'tWgaps1lklk8Ax707dqK8c7P1kcyj0CYObg1oEuHO7h71';
+})();
